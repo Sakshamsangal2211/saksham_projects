85292a3d1ae921e177e9c0a262b1b3cb80dc4838
saksham <sakshamsangall2211@gmail.com>
2022-03-26 17:47:22 +0530

    hello world

---
+++ app/src/main/java/com/DataFlair/mycalculator/MainActivity.java
@@ -0,0 +1,241 @@
+package com.DataFlair.mycalculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+//import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+
+public class MainActivity extends AppCompatActivity {
+//    public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+
+    double in1 = 0, i2 = 0, tot = 0, MRP = 0;
+    TextView edittext1;
+    boolean Add, Sub, Multiply, Divide, Remainder, deci;
+    Button button_0, button_1, button_2, button_3, button_4, button_5, button_6, button_7, button_8, button_9, button_Add, button_Sub,
+            button_Mul, button_Div, button_Equ, button_Del, button_Dot, button_Remainder;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        button_0 = (Button) findViewById(R.id.b0);
+        button_1 = (Button) findViewById(R.id.b1);
+        button_2 = (Button) findViewById(R.id.b2);
+        button_3 = (Button) findViewById(R.id.b3);
+        button_4 = (Button) findViewById(R.id.b4);
+        button_5 = (Button) findViewById(R.id.b5);
+        button_6 = (Button) findViewById(R.id.b6);
+        button_7 = (Button) findViewById(R.id.b7);
+        button_8 = (Button) findViewById(R.id.b8);
+        button_9 = (Button) findViewById(R.id.b9);
+        button_Dot = (Button) findViewById(R.id.bDot);
+        button_Add = (Button) findViewById(R.id.badd);
+        button_Sub = (Button) findViewById(R.id.bsub);
+        button_Mul = (Button) findViewById(R.id.bmul);
+        button_Div = (Button) findViewById(R.id.biv);
+        button_Remainder = (Button) findViewById(R.id.BRemain);
+        button_Del = (Button) findViewById(R.id.buttonDel);
+        button_Equ = (Button) findViewById(R.id.buttoneql);
+
+        edittext1 = (TextView) findViewById(R.id.textView);
+
+
+        button_1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                edittext1.setText(edittext1.getText() + "1");
+            }
+        });
+
+        button_2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                edittext1.setText(edittext1.getText() + "2");
+            }
+        });
+
+        button_3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                edittext1.setText(edittext1.getText() + "3");
+            }
+        });
+
+        button_4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                edittext1.setText(edittext1.getText() + "4");
+            }
+        });
+
+        button_5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                edittext1.setText(edittext1.getText() + "5");
+            }
+        });
+
+        button_6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                edittext1.setText(edittext1.getText() + "6");
+            }
+        });
+
+        button_7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                edittext1.setText(edittext1.getText() + "7");
+            }
+        });
+
+        button_8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                edittext1.setText(edittext1.getText() + "8");
+            }
+        });
+
+        button_9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                edittext1.setText(edittext1.getText() + "9");
+            }
+        });
+
+        button_0.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                edittext1.setText(edittext1.getText() + "0");
+            }
+        });
+
+        button_Add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (edittext1.getText().length() != 0) {
+                    in1 = Float.parseFloat(edittext1.getText() + "");
+                    tot = 0.75 * in1 + tot;
+                    MRP = MRP + in1;
+                    Add = true;
+                    deci = false;
+                    edittext1.setText(null);
+                }
+
+            }
+        });
+
+        button_Sub.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (edittext1.getText().length() != 0) {
+                    in1 = Float.parseFloat(edittext1.getText() + "");
+                    Sub = true;
+                    deci = false;
+                    edittext1.setText(null);
+                }
+            }
+        });
+
+        button_Mul.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (edittext1.getText().length() != 0) {
+                    in1 = Float.parseFloat(edittext1.getText() + "");
+                    Multiply = true;
+                    deci = false;
+                    edittext1.setText(null);
+                }
+            }
+        });
+
+        button_Div.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                edittext1.setText("MRP SUM :" + MRP);
+            }
+
+        });
+
+        button_Remainder.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                edittext1.setText("");
+
+
+            }
+        });
+
+        button_Equ.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (Add || Sub || Multiply || Divide || Remainder) {
+                    i2 = Float.parseFloat(edittext1.getText() + "");
+                }
+
+                if (Add) {
+                    tot = tot + 0.75 * (Float.parseFloat(edittext1.getText() + ""));
+                    MRP = MRP + (Float.parseFloat(edittext1.getText() + ""));
+
+                    edittext1.setText("NET PAYABLE AMOUNT :" + tot);
+                    Add = false;
+                }
+
+                if (Sub) {
+
+                    edittext1.setText(in1 - i2 + "");
+                    Sub = false;
+                }
+
+                if (Multiply) {
+                    edittext1.setText(in1 * i2 + "");
+                    Multiply = false;
+                }
+
+                if (Divide) {
+                    edittext1.setText(in1 / i2 + "");
+                    Divide = false;
+                }
+                if (Remainder) {
+                    edittext1.setText(in1 % i2 + "");
+                    Remainder = false;
+                }
+            }
+        });
+
+        button_Del.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                edittext1.setText("");
+                in1 = 0.0;
+                i2 = 0.0;
+            }
+        });
+
+        button_Dot.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                edittext1.setText("THANK YOU , HAVE A NICE DAY");
+
+            }
+        });
+
+
+    }
+
+//    public void sendMessage(View view) {
+//        Intent intent = new Intent(this,MainActivity2.class);
+//        String message = edittext1.getText().toString();
+//        intent.putExtra(EXTRA_MESSAGE, message);
+//        startActivity(intent);
+//    }
+}
+
